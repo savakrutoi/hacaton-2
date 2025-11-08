59739aa26db548c92afabce32d852440a0e081ed
saveliy <chebonenko6@mail.ru>
2025-11-08 18:14:52 +0300

    modeli прописал следующий шаг думаем о маштабируемости

--- osnovnoe.py
+++ osnovnoe.py
@@ -1,5 +1,35 @@
 from typing import Union
 from fastapi import FastAPI
-from fastapi import
+from pydantic.v1 import BaseModel
+from starlette.authentication import BaseUser
+from enum import Enum
+from datetime import date, datetime
 
-class User()
+class Role(str, Enum): # это просто строка с ограниченными значениями
+    stutent = 'student'
+    teacher = "teacher"
+    headman = "headman"
+class User(BaseModel):
+    id: int
+    name: str
+    role: Role # поле role должно принимать значение из Role (и когда мы будет кидать запрос мы будет автомато проверять есть ли слово после / в этой строке)
+
+#CЕЙЧАС РАБОТАЮ С НЕЙРОНКОЙ И ДУМАЮ НАД МАШТАБИРУЕМОСТЬЮ В ТЕОРИИ ВНЕСУ ПОЛЯ В User ДЛЯ РЕГЕСТИТРАЦИИ ТАМ ПОЧТА И ТАК ДАЛЕЕ
+
+
+class Visit(BaseModel):
+    id_visit: int
+    name_visit: str
+    visits: bool
+
+class Lesson(BaseModel):
+    id: int
+    name_lesson: str
+    data_created: date # вот это я прям сам без нейронки чувствую себя бил гейтсом
+
+#УЖЕ НАЧИНАТЬ РАБОАТЬ С БАЗОЙ ДАННЫХ sql alchimy
+
+
+    created_by: int #кто создал староста или препод
+
+
